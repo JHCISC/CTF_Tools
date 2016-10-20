080a7af960e2f5dacbb971f46621c8887ccc6918
andyongxa <andyong1996@gmail.com>
2016-10-20 15:52:00 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 import CTFEZ
 
 for i in range(0, 1000):
+    print('Copyright © 2016 JHCISC SONY FANS. ')
     print("===========================================================")
     print('0#退出程序\n1#ascii码转化\n2#二进制转化为字符\n3#base64解码\n4#摩斯密码\n5#栅栏密码\n6#凯撒密码\n7#培根密码\n8#当铺密码')
     number = int(input("请输入选择的功能: "))
